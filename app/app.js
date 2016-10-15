df95c7baf41898f92353ed4bfd37f0a0fee002e5
david <david@akademia.co.jp>
2016-10-16 01:58:03 +0900

    update package.json

--- app/app.js
+++ app/app.js
@@ -1,20 +1,20 @@
 //React libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 //Import Container component
 import Page1 from './containers/page1'
 
 class App extends React.Component {
-  render () {
-    return (
-      <Page1 />
-    );
-  }
+    render () {
+        return (
+            <Page1 />
+        )
+    }
 }
 
 // Render to index.html
 ReactDOM.render(
-  <App />,
+  <App/>,
   document.getElementById('content')
-);
+)
